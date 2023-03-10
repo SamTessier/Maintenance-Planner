111b34ca2c8f5f96776d3b85dd1df2cac8f24657
SamTessier <samcjtessier@gmail.com>
2023-03-10 02:36:54 -0330

    UI Continuity fixes
    added and fixed popovers, aligned button colors and styles between all modals, added help icons to add vehicle modal

--- view-vehicle.js
+++ view-vehicle.js
@@ -1,68 +1,66 @@
 function populateViewModal(vehicleId, vehicleObject) {
-    console.log(
-      `opening vehicle row `,
-      vehicleObject,
-      Object.entries(vehicleObject["workOrders"])
-    );
-  
-    $("#view-vehicle-modal").show().dimBackground();
-    $("ul#view-modal-vehicle-property-list").empty();
-  
-    for (const [key, value] of Object.entries(vehicleObject)) {
-      let displayKey = key;
-      let newValue = value;
-      if (key === "vStatus") {
-        displayKey = "Status";
-        if (value.toString() === "true") {
-          newValue = "In Service";
-        } else if (value.toString() === "false") {
-          newValue = "Out of Service";
-        }
-      } else if (key === "name") {
-        displayKey = "Vehicle";
-      }else if (key === "workOrders") {
-        displayKey = "Work Orders";
-        if (vehicleObject["workOrders"].size > 0) {
-          newValue = "!Open Work Orders!";
-        } else {
-          newValue = "No Work Orders Open";
-        }
-      }
-      
-      
+  console.log(
+    `opening vehicle row `,
+    vehicleObject,
+    Object.entries(vehicleObject["workOrders"])
+  );
 
-      $("ul#view-modal-vehicle-property-list").append(`
+  $("#view-vehicle-modal").show().dimBackground();
+  $("ul#view-modal-vehicle-property-list").empty();
+
+  for (const [key, value] of Object.entries(vehicleObject)) {
+    let displayKey = key;
+    let newValue = value;
+    if (key === "vStatus") {
+      displayKey = "Status";
+      if (value.toString() === "true") {
+        newValue = "In Service";
+      } else if (value.toString() === "false") {
+        newValue = "Out of Service";
+      }
+    } else if (key === "name") {
+      displayKey = "Vehicle";
+    } else if (key === "workOrders") {
+      displayKey = "Work Orders";
+      if (vehicleObject["workOrders"].size > 0) {
+        newValue = "!Open Work Orders!";
+      } else {
+        newValue = "No Work Orders Open";
+      }
+    }
+
+    $("ul#view-modal-vehicle-property-list").append(`
         <li>
           <strong> ${displayKey} : </strong>
           <span>  ${newValue} </span> 
         </li>
       `);
-    }
-  
-    $("tbody#work-order-table-body").html("");
-    console.log(
-      "Iterating WorkOrders: ",
-      vehicleObject["workOrders"],
-      vehicleObject["workOrders"].entries(),
-      Object.entries(vehicleObject["workOrders"])
-    );
-    for (const [workOrderId, workOrderObject] of vehicleObject["workOrders"].entries()) {
-      console.log("Creating work order row: ", workOrderId, workOrderObject);
-      $("tbody#work-order-table-body").append(`
+  }
+
+  $("tbody#work-order-table-body").html("");
+  console.log(
+    "Iterating WorkOrders: ",
+    vehicleObject["workOrders"],
+    vehicleObject["workOrders"].entries(),
+    Object.entries(vehicleObject["workOrders"])
+  );
+  for (const [workOrderId, workOrderObject] of vehicleObject[
+    "workOrders"
+  ].entries()) {
+    console.log("Creating work order row: ", workOrderId, workOrderObject);
+    $("tbody#work-order-table-body").append(`
         <tr> 
           <td>${workOrderId}</td>
           <td>${workOrderObject["description"]}</td>
           <td>${workOrderObject["actionRequired"]}</td>
         </tr>
       `);
-    }
   }
-  
+}
 
 function perRowViewPropertiesClickEvent(vehicleId, vehicleObject) {
-  $(`button#vehicle-view-${vehicleObject.name}`).click(function(){
+  $(`button#vehicle-view-${vehicleObject.name}`).click(function () {
     populateViewModal(vehicleId, vehicleObject);
   });
   selectedVehicleObjectId = vehicleId;
 }
-
